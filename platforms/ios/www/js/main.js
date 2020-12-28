95f1a8c73775d7005a80463f21ec4bc1c9801941
Selenita13 <70034388+Selenita13@users.noreply.github.com>
2020-12-28 10:27:47 -0700

    debug 6
    ionic cordova platform add ios

---
+++ platforms/ios/www/js/main.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+
+
+$(document).ready(function() {
+    
+	
+	// Recargar contenido en el segundo touch
+	document.querySelector('#tabbar').addEventListener('reactive', function() { 
+		var tab_index = document.querySelector('#tabbar').getActiveTabIndex();
+		if(tab_index == 2){
+			//loadElements();
+		}else if(tab_index == 4){
+			//loadNotifications();
+		}	
+	});
+	
+	$(document).on(clickHandler, "textarea", function(){
+		"use strict";
+		
+		if(!touchmoved){	
+				console.log('touched3');
+			$(this).focus();
+		}
+	}).on('touchmove', function(e){
+		"use strict";
+		touchmoved = true;
+	}).on('touchstart', function(){
+		"use strict";
+		touchmoved = false;
+	});
+
+});
+
+//Cambio de tamaño en la vetana
+function thisResize() {
+	
+}
+
+var resizeTimer; $(window).resize(function () { if (resizeTimer) { clearTimeout(resizeTimer); } resizeTimer = setTimeout(function() { resizeTimer = null; thisResize(); }, 500);});
