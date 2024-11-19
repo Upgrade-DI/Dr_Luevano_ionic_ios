41fe3520cd373782c1b498f8ccc120b6c3fd8ede
Eduardo Navarrete <eduardo@upgrade.com.mx>
2024-11-19 08:32:51 -0600

    Add pushwoosh notifications

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',
+  root: 'src',
   build: {
-    outDir: '../dist',
-    minify: false,
-    emptyOutDir: true,
-  },
+    outDir: 'src', // Esto asegura que los archivos generados estén en www
+    emptyOutDir: false // Para evitar que Vite elimine accidentalmente la carpeta www
+  }
 });
