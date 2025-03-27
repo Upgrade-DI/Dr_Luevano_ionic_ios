8238b8ec7fc0d6bfbefcda8024a5e32939eb0e31
Eduardo Navarrete <eduardo@upgrade.com.mx>
2025-03-27 08:55:11 -0600

    test send token device bd

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,11 +9,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PWMessagingDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // CORREGIDO: Inicializar Pushwoosh con el método correcto
+        Pushwoosh.sharedInstance().initializeWithAppCode("DE31D-FA23F", appName: "Armonia")
+        
         Pushwoosh.sharedInstance().delegate = self
-        
-        // AÑADIDO: Configurar el App ID de Pushwoosh
-        Pushwoosh.sharedInstance().applicationCode = "DE31D-FA23F"
-        
         Pushwoosh.sharedInstance().registerForPushNotifications()
         
         // AÑADIDO: Registrar para notificaciones cuando el ID del paciente cambie
@@ -27,40 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PWMessagingDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        // Called when the app was launched with a url. Feel free to add additional processing here,
-        // but if you want the App API to support tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
-    }
-
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
-    }
+    // Resto de métodos del ciclo de vida...
     
     //handle token received from APNS
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
@@ -79,27 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PWMessagingDelegate {
         checkAndSendToken()
     }
 
-    //handle token receiving error
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("❌ Error al registrar para notificaciones: \(error.localizedDescription)")
-        Pushwoosh.sharedInstance().handlePushRegistrationFailure(error)
-    }
-
-    //this is for iOS < 10 and for silent push notifications
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        Pushwoosh.sharedInstance().handlePushReceived(userInfo)
-        completionHandler(.noData)
-    }
-
-    //this event is fired when the push gets received
-    func pushwoosh(_ pushwoosh: Pushwoosh, onMessageReceived message: PWMessage) {
-        print("onMessageReceived: ", message.payload!.description)
-    }
-
-    // Fired when a user taps the notification
-    func pushwoosh(_ pushwoosh: Pushwoosh, onMessageOpened message: PWMessage) {
-        print("onMessageOpened: ", message.payload!.description)
-    }
+    // Resto de métodos para manejar notificaciones...
     
     // AÑADIDO: Método para verificar y enviar el token
     func checkAndSendToken() {
